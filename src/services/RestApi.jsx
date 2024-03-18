9dd4bf4e754fa14ea634ba7571ca5233fb107bba
Arleny <arleny.ares@gmail.com>
2024-03-18 12:39:49 +0100

    checking axios and the back

--- src/services/RestApi.jsx
+++ src/services/RestApi.jsx
@@ -6,7 +6,7 @@ axios.defaults.headers.post['Accept'] = 'application/json';
 export const restapi = () => {
     //servicio para consumir un endpoint
 
-    const baseUrl = "http://127.0.0.1:8000/api";
+    const baseUrl = "http://127.0.0.1:8000/api/";
     const getAll = async () => {
         const response = await axios.get(baseUrl);
         return response;
