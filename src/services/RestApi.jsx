9bb09eb699ab3a2ecb44d777080d74f44b3120b5
Arleny <arleny.ares@gmail.com>
2024-03-22 15:09:55 +0100

    config proxy

--- src/services/RestApi.jsx
+++ src/services/RestApi.jsx
@@ -8,7 +8,7 @@ axios.defaults.withXSRFToken = true;
 export const restapi = () => {
     //servicio para consumir un endpoint
 
-    const baseUrl = "http://127.0.0.1:8000/api/";
+    const baseUrl = "http://127.0.0.1:8000/";
     const getAll = async () => {
         const response = await axios.get(baseUrl);
         return response;
