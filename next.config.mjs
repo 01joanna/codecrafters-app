14038e847d7afec5cbb479b1245369d6c11dfdc2
Arleny <arleny.ares@gmail.com>
2024-03-23 13:11:49 +0100

    axios correction, works correctly

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+const nextConfig = {
+  images: {
+    domains: ['source.unsplash.com'], // Agrega 'source.unsplash.com' a la lista de dominios permitidos
+  },
+};
 
 export default nextConfig;
--- src/app/components/Card/Card.jsx
+++ src/app/components/Card/Card.jsx
@@ -1,50 +1,56 @@
-"use client"
+'use client'
 import Image from "next/image";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { FaUsers } from "react-icons/fa6";
 import { IoEarthSharp } from "react-icons/io5";
-import { restapi } from '@/services/RestApi.jsx';
+
+import restapi from "@/services/RestApi.jsx";
 
 
 export default function Card() {
-
-    // Llamar al servicio
-    // Almacenamos esa respuesta dentro de un estado
-    // Renderizar el resultado
-
-    // const service = restapi();
+    const [events, setEvents] = useState([]);
 
     useEffect(() => {
         const service = restapi();
-        service.getAll().then(response => {
-            console.log(response);
-        }).catch(error => {
-            console.log(error);
+        service
+        .getAllEvents()
+        .then((response) => {
+            setEvents(response.data); // Almacena los eventos en el estado local
         })
-    })
+        .catch((error) => {
+            console.log(error);
+        });
+    }, []);
 
     return (
-        <div className="bg-white">
-            <Image 
-            src="/img/rectangle.png"
-            alt="Event picture"
-            width={250}
-            height={250}
+        <div>
+        {events.map((event) => (
+            <div key={event.id} className="bg-white">
+            <Image
+                src={event.image}
+                alt="Event picture"
+                width={250}
+                height={250}
             />
-            <div id="card-text" className="text-black flex flex-col gap-1 w-[170px]">
+            <div
+                id="card-text"
+                className="text-black flex flex-col gap-1 w-[170px]"
+            >
                 <div className="flex gap-3 items-center">
-                    <h1 className="text-lg">Event name</h1>
-                    <IoEarthSharp />
+                <h1 className="text-lg">{event.title}</h1>
+                <IoEarthSharp />
                 </div>
                 <div className="flex gap-3 text-xs uppercase">
-                    <h2 className="text-gray-400">18:00h</h2>
-                    <h3>OCT 13, 24 | BCN</h3>
+                <h2 className="text-gray-400">{event.date}</h2>
+                <h3>{event.location}</h3>
                 </div>
                 <span className="flex gap-2 items-center text-xs">
-                    <FaUsers />
-                    <h6>10 assistants</h6>
+                <FaUsers />
+                <h6>{event.attendees_count} assistants</h6>
                 </span>
             </div>
+            </div>
+        ))}
         </div>
-    )
+    );
 }
