32770282467241389f29377d36bec83145228e4f
01joanna <joa.cuevas9@gmail.com>
2024-03-30 21:40:02 +0100

    card shows different icons if event is yours

--- src/app/components/Card/Card.jsx
+++ src/app/components/Card/Card.jsx
@@ -4,14 +4,30 @@ import React from "react";
 import { useRouter } from "next/navigation";
 import { FaUsers } from "react-icons/fa6";
 import { IoEarthSharp } from "react-icons/io5";
+import { MdDelete } from "react-icons/md";
+import { CiEdit } from "react-icons/ci";
+import { useAuthContext } from "@/contexts/AuthContext";
 
 
 export default function Card({ event, className }) {
+
+  const { isAuthenticated, getUserData } = useAuthContext();
+  const userData = getUserData();
+  const isOwner = isAuthenticated && userData && event.user_id === userData.id;
 
   const router = useRouter();
   const HandleViewDetails = () => {
     router.push(`/events/${event.id}`);
   }
+
+  const handleEditEvent = () => {
+    router.push(`/events/${event.id}`);
+  };
+
+  const handleDeleteEvent = () => {
+    // Lógica para eliminar el evento
+  };
+
   
   const defaultCSS = "bg-white flex flex-col gap-4"
   return (
@@ -38,6 +54,18 @@ export default function Card({ event, className }) {
         <button 
         className="bg-lightmayonnaise text-black text-[9px] py-1 px-2 rounded-md mt-4"
         onClick={HandleViewDetails}>View Event Details</button>
+        {isOwner && (
+          <div className="flex gap-2 mt-2">
+            <CiEdit 
+              className="text-blue-500 cursor-pointer"
+              onClick={handleEditEvent}
+            />
+            <MdDelete
+              className="text-red-500 cursor-pointer"
+              onClick={handleDeleteEvent}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
